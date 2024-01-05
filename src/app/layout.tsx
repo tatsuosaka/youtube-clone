7a1c2ed5950b8f1c6ab9a63c39d944f462a209be
tatsuosaka <matheus.tats@gmail.com>
2024-01-04 21:49:48 -0300

    Layout Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Youtube Clone',
+  title: 'YouTube Clone',
   description: 'Clone do YouTube Front-End com TypeScript, React, Next.js e Tailwind CSS',
 }
 
